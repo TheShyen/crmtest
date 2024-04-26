d66212f45974c9f2a620bca9f4c378c752172bdf
Ruslan <ruslankasles@gmail.com>
2024-04-26 15:56:31 +0500

    d

--- app/resources/js/components/Buttons/dropdownButton.vue
+++ app/resources/js/components/Buttons/dropdownButton.vue
@@ -1,13 +1,15 @@
 <template>
-  <q-btn-dropdown icon="arrow">
-    <slot/>
+  <q-btn-dropdown push rounded dense no-caps flat :label="label" icon="arrow">
+    <slot></slot>
   </q-btn-dropdown>
 </template>
 
 <script setup>
+import { defineProps } from "vue";
 
+const props = defineProps({
+  label: String,
+});
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
